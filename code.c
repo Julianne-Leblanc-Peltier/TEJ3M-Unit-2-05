5c8ee1a08302a285255eac4339d63db5548268f2
Julianne <144264326+Julianne-Leblanc-Peltier@users.noreply.github.com>
2025-04-18 05:13:29 +0000

    added code, need to fix! :)

--- code.c
+++ code.c
@@ -7,17 +7,26 @@
 
  # include <Servo.h>
 
- Servo servoNumber1;
+ Servo servo;
+ int angle = 10;
  int blinkTime = 1000;
    
   void setup() 
  {
-    servoNumber1.attach(2);
-    servoNumber1.write(0);
+    servo.attach(2);
+    servo.write(angle);
   }
   
   void loop() 
  {
+    // scan from 0  to 180 degrees
+    for (angle = 10; angle < 180; angle++)
+    {
+      servo.write(angle);
+      delay(15);
+    }
+    // now scan back from 180 to 0 degrees
+    for(angle 180; angle > 10; angle--)
     servoNumber1.write(90);
     delay(blinkTime);
     servoNumber1.write(0);
